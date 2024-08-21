7d91bb5909025a8fb4ed2786b68af466d3b1c9d6
Pru-Se <surattipong@gmail.com>
2024-08-21 15:05:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ if classifier == 'SVM':
   svm.fit(x_train, y_train)
   y_pred = svm.predict(x_test)
   acc = accuracy_score(y_test, y_pred)
+  st.write(acc)
 if classifier == 'RF':
   rf = RandomForestClassifier(n_estimators=100)
   rf.fit(x_train, y_train)
