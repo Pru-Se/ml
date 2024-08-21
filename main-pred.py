3499ebaedf88f4e82fa3ad41c8618cbaae745e8e
Pru-Se <surattipong@gmail.com>
2024-08-21 16:19:56 +0700

    Update main-pred.py

--- main-pred.py
+++ main-pred.py
@@ -4,13 +4,13 @@ x = np.random.rand(100)
 y = 2 * x + 1 + 0.2* np.random.randn(100)
 plt.scatter(x, y)
 
-from sklearn.neural_network import MLPRegressor
-nn = MLPRegressor(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
-nn.fit(x.reshape(-1, 1), y)
+#from sklearn.neural_network import MLPRegressor
+#nn = MLPRegressor(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
+#nn.fit(x.reshape(-1, 1), y)
 
-y_pred = nn.predict(x.reshape(-1, 1))
+#y_pred = nn.predict(x.reshape(-1, 1))
 
-fig, ax = plt.subplots()
-ax.scatter(x, y)
-ax.scatter(x, y_pred)
-st.pyplot(fig)
+#fig, ax = plt.subplots()
+#ax.scatter(x, y)
+#ax.scatter(x, y_pred)
+#st.pyplot(fig)
