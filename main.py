c30bfb4060e127a232753e28cd8bc6780969ffce
Pru-Se <surattipong@gmail.com>
2024-08-21 15:01:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ y = df['species']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 st.sidebar.title('Classifiers')
-classifier = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM'))
+classifier = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM', 'RF'))
 k = st.sidebar.slider('K', 1, 120, 3)
 if classifier == 'KNN':
   knn = KNeighborsClassifier(n_neighbors=3)
@@ -26,5 +26,10 @@ if classifier == 'SVM':
   svm = SVC()
   svm.fit(x_train, y_train)
   y_pred = svm.predict(x_test)
+  acc = accuracy_score(y_test, y_pred)
+  if classifier == 'RF':
+  rf = RandomForestClassifier(n_estimators=100)
+  rf.fit(x_train, y_train)
+  y_pred = rf.predict(x_test)
   acc = accuracy_score(y_test, y_pred)
   st.write(acc)
